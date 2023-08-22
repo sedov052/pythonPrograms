177e84f201752a3c0b1960181816816143afef1a
sedov052 <sedas052285@gmail.com>
2023-08-23 03:22:20 +1000

    Исправлено отображение ссылки.

--- parsingKworksWants.py
+++ parsingKworksWants.py
@@ -5,6 +5,6 @@ while True:
         response = requests.post("https://kwork.ru/projects").json()
         orders = response["data"]["wants"]
         for order in orders:
-            file.write(f"{order['name']} ({order['kworkCount']} Предложений) -> {order['priceLimit']} - https://kwork.ru{order['url'][1:]}\n")
+            file.write(f"{order['name']} ({order['kworkCount']} Предложений) -> {order['priceLimit']} - https://kwork.ru/{order['url'][1:]}\n")
             file.write(f"{order['description']}\n\n")
     sleep(60)
